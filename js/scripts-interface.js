e46f6770f05d40ad73532615d77cab1459ac210f
Connor Larsen and Rose Sponseller <rose@>
2017-06-29 09:43:32 -0700

    Start fresh in a new working directory

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -6,6 +6,7 @@ $(function() {
     e.preventDefault();
 
     var searchInput = $('.search-input').val();
+    var image;
 
     $('.search-input').val('');
 
@@ -18,38 +19,31 @@ $(function() {
       if (JSON.stringify(response.data[0].ibu) === undefined) {
         response.data[0].ibu = "N/A";
       }
+      if (response.data[0].hasOwnProperty('labels') === false) {
+        image = "Image coming soon";
+      } else {
+        // image = response.data[0].labels.medium;
+        image = '<img src="' + response.data[0].labels.medium + '">';
+      }
+
 
       console.log(JSON.stringify(response));
       $('.result').html(
+        // Display Image
+        image +
+
         // Drink searched, ABV and IBU info
         "<h3>Drink searched: " + searchInput + "</h3><br>" +
         "<ul><li><strong>ABV: </strong>" + response.data[0].abv + "%</li>" +
         "<li><strong>IBU: </strong>" + response.data[0].ibu + "</li>" +
         "<li><strong>Organic: </strong>" + response.data[0].isOrganic + "</li></ul>" +
 
-
         // Drink category and description
         "<h3>Drink category: " + response.data[0].style.category.name + "</h3><br>" +
         "<p><strong>Description:</strong></p><p>" + response.data[0].style.description + "</p>" +
-
-        // Image
-        '<img src="' + response.data[0].labels.medium + '">'
-
-        // "<li><strong>Organic: </strong>" + organicResult + "</li>"
-
-        // (JSON.stringify(response.data[0].isOrganic)) + "</li></ul>"
-        // organic
-        // (JSON.stringify(response.data[0].style.description))
-        // + "<br>" +
-        // (JSON.stringify(response.data[0].style.id))
+        "<p><strong>ABV Range: </strong>" + response.data[0].style.abvMin + "-" + response.data[0].style.abvMax + "</p>" +
+        "<p><strong>IBU Range: </strong>" + response.data[0].style.ibuMin + "-" + response.data[0].style.ibuMax + "</p>"
       );
     });
   });
 });
-
-// (JSON.parse(response.data[0].ibu))
-// "</li><br><li><strong>Organic: </strong>" +
-// (JSON.parse(response.data[0].isOrganic))
-// (JSON.parse(response.data[0].style.description))
-// + "<br>" +
-// (JSON.stringify(response.data[0].style.id))
