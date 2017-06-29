5e275ab89852c73b97f6ca5ac1d6b67542e90219
Rose Sponseller <rosesponseller@gmail.com>
2017-06-28 20:16:25 -0700

    Add successfull if statements for undefined scenarios, displays all descriptions and titles, images work -- but the result will not work if there is no image

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -6,34 +6,42 @@ $(function() {
     e.preventDefault();
 
     var searchInput = $('.search-input').val();
-    var organicResult;
 
     $('.search-input').val('');
 
     $.get("http://api.brewerydb.com/v2/beers/?name=" + searchInput + "&description&key=" + apiKey, function(response) {
 
-      // function organic() {
-      //   if (JSON.stringify(response.data[0].isOrganic) === true) {
-      //     organicResult = "Yes";
-      //   } else {
-      //     organicResult = "No";
-      //   }
-      // };
+      // If isOrganic and IBU are undefined, display N/A
+      if (JSON.stringify(response.data[0].isOrganic) === undefined) {
+        response.data[0].isOrganic = "N/A";
+      }
+      if (JSON.stringify(response.data[0].ibu) === undefined) {
+        response.data[0].ibu = "N/A";
+      }
 
       console.log(JSON.stringify(response));
       $('.result').html(
+        // Drink searched, ABV and IBU info
         "<h3>Drink searched: " + searchInput + "</h3><br>" +
-        "<ul><li><strong>ABV: </strong>" + (JSON.parse(response.data[0].abv)) + "%</li>" +
-        "<li><strong>IBU: </strong>" + (JSON.parse(response.data[0].ibu)) + "</li></ul>" +
+        "<ul><li><strong>ABV: </strong>" + response.data[0].abv + "%</li>" +
+        "<li><strong>IBU: </strong>" + response.data[0].ibu + "</li>" +
+        "<li><strong>Organic: </strong>" + response.data[0].isOrganic + "</li></ul>" +
+
+
+        // Drink category and description
+        "<h3>Drink category: " + response.data[0].style.category.name + "</h3><br>" +
+        "<p><strong>Description:</strong></p><p>" + response.data[0].style.description + "</p>" +
+
+        // Image
+        '<img src="' + response.data[0].labels.medium + '">'
+
         // "<li><strong>Organic: </strong>" + organicResult + "</li>"
-        "<h3>Drink category: " + searchInput + "</h3><br>"
 
         // (JSON.stringify(response.data[0].isOrganic)) + "</li></ul>"
         // organic
         // (JSON.stringify(response.data[0].style.description))
         // + "<br>" +
         // (JSON.stringify(response.data[0].style.id))
-
       );
     });
   });
